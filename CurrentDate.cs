1d44c02d3cd1cd1da24d65aa7f82ad8f516ab0a0
Cameron Clark <cameron.clark31@gmail.com>
2023-03-01 13:49:10 -0500

    More changes to CurrentDate Moore EF5 calculation

--- CurrentDate.cs
+++ CurrentDate.cs
@@ -13,11 +13,11 @@ namespace SevereWeatherPrep
         {
             DateTime mooreTornado = new DateTime(2013, 05, 20);
 
-            Console.WriteLine(mooreTornado.ToString() + " - This is the date of the Moore, OK EF5 Tornado\n"); 
+            
 
             Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt") + " - Is the current Time\n");
-
-            Console.WriteLine((mooreTornado.Day - DateTime.Now.Day) + " " + mooreTornado.Day);
+            Console.WriteLine(mooreTornado.ToString() + " - Date date of the Moore, OK EF5 Tornado\n");
+            Console.WriteLine("it has only been " + (DateTime.Now.Year - mooreTornado.Year) + " years since the Moore EF5 tornado. Are you prepared?\n");
 
             //(EndDate - StartDate).TotalDays
             //however if you would like only WHOLE days as an int and are happy to forgo the time component of the date then consider:
